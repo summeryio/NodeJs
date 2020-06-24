f3a24dd3b6b1e72c3df0cd3d5451fa27f272b8f7
summeryio <1005526074@qq.com>
2020-06-24 18:16:45 +0800

    express 中间件

--- hello-express/server.js
+++ hello-express/server.js
@@ -4,11 +4,18 @@ const fs = require('fs')
 const multer = require('multer')
 
 const app = express()
-app.set('view engine', 'ejs')
 
-app.get('/user/:name', (req, res) => {
-    const person = req.params.name
-    res.render('user', {person: person})
+// 访问静态文件
+app.use('/static', express.static('static'))
+
+app.use((req, res, next) => {
+    console.log('first middleware');
+    next()
+    console.log('first middleware after');
+})
+app.get('/', (req, res) => {
+    console.log('second middleware');
+    res.send('ok')
 })
 
 app.listen(3000)
