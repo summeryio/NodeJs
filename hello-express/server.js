865d2cf4f356bb5bd6bd61ec0e354317325f7af6
summeryio <summeryio@summeryiodeiMac.local>
2020-06-25 11:23:26 +0800

    路由中间件

--- hello-express/server.js
+++ hello-express/server.js
@@ -1,21 +1,11 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const fs = require('fs')
-const multer = require('multer')
-
 const app = express()
 
-// 访问静态文件
-app.use('/static', express.static('static'))
+const indexRouter = require('./routes/index')
+const userRouter = require('./routes/user')
 
-app.use((req, res, next) => {
-    console.log('first middleware');
-    next()
-    console.log('first middleware after');
-})
-app.get('/', (req, res) => {
-    console.log('second middleware');
-    res.send('ok')
-})
+app.use('/', indexRouter)
+app.use('/user', userRouter)
+
 
 app.listen(3000)
